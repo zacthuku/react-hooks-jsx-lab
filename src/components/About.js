bfdf4b999cd10963ea61fdc20f1548909fa8fc14
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-11 10:26:50 +0300

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I’m passionate about building things on the web and always eager to learn new technologies!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
